99398e7c378a778c8634b058e639f4a471cc9373
Cosmas-ogo <anene.ogochukwu@yahoo.com>
2025-12-02 23:28:34 +0000

    test: add tests for task creation endpoint

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+} else {
+  dotenv.config();
+}
 
 const connectionString =
   process.env.DATABASE_URL ||
-  "postgres://user:password@localhost:5432/hmcts_tasks";
+  (process.env.NODE_ENV === "test"
+    ? "postgres:///hmcts_tasks_test"
+    : "postgres:///hmcts_tasks");
 
-const pool = new Pool({
-  connectionString,
-});
+const pool = new Pool({ connectionString });
 
 pool.on("error", (err) => {
   console.error("Unexpected error on idle Postgres client", err);
